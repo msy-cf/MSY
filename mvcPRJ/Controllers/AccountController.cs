03a46333620b1812493e7ba383bba95970e3ef13
mahamatsaleh <mahamats24@gmail.com>
2019-08-28 14:34:34 +0300

    This projet is to Create a register and Login page

---
+++ mvcPRJ/Controllers/AccountController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvcPRJ.Models;
+
+namespace mvcPRJ.Controllers
+{
+    public class AccountController : Controller
+    {
+        private const string V = " Registred Successfuly";
+
+        // GET: Account
+        public ActionResult Index()
+        {   
+            using (OurDbContext db = new OurDbContext())
+           { 
+             return View(db.UserAccount.ToList());
+            }
+        }
+
+        public ActionResult Register()
+            {
+            return View();
+             }
+
+        [HttpPost]
+        public ActionResult Register(UserAccount account)
+            {
+            if(ModelState.IsValid)
+                {
+                    using (OurDbContext db = new OurDbContext())
+                    { db.UserAccount.Add(account);
+                        db.SaveChanges();
+                    }
+                    ModelState.Clear();
+                ViewBag.Message= account.FirstName + "  " + account.LastName + V;
+                 }
+            return View();
+}
+        //Login
+        public ActionResult Login()
+            { 
+              return View();
+            }
+         
+        [HttpPost]
+        public ActionResult Login(UserAccount user)
+            {
+                  using (OurDbContext db  = new OurDbContext())
+                  {
+                       var usr = db.UserAccount.SingleOrDefault(u => u.UserName == user.UserName && u.Password == user.Password);
+                      if(usr != null)
+                      {
+                          Session["UserID"]=usr.UserID.ToString();
+                          Session["UserName"]= usr.UserName.ToString();
+                          return RedirectToAction("LoggedIn");
+                       }
+                      else
+                       {
+                           NewMethod();
+                       }
+                
+                  }   
+                  return View();   
+             }
+
+        private void NewMethod()
+        {
+            ModelState.AddModelError(key: "", errorMessage: "Username or Password is invalid try again.");
+        }
+
+        public ActionResult LoggedIn()
+            {
+               if(Session["UserId"]!=null)
+                {   return  View(); }
+               else {return RedirectToAction("Login");}
+            }
+    }
+}
---
+++ mvcPRJ/Models/UserAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace mvcPRJ.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int UserID { get; set; }
+        [Required (ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [RegularExpression(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-A]{2,3}|[0-9]{1,3})(\]?)$", ErrorMessage ="Please enter valid email")]
+        public string Email { get; set; }
+        
+        [Required(ErrorMessage = "Username is required")]
+        public string UserName { get; set; }
+
+        [Required (ErrorMessage ="Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password",ErrorMessage ="Please confirm password.")]
+        [DataType(DataType.Password)]
+
+        public string ConfirmPassword { get; set; }
+    }
+}
